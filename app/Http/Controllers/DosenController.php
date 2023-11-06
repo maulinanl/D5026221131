ad482c7cf34a094d1478a63251342a63373e36eb
maulinanl <maulina.2034@gmail.com>
2023-11-06 15:33:22 +0700

    initial commit

---
+++ app/Http/Controllers/DosenController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DosenController extends Controller
+{
+    //
+public function index(){
+    $a = 3;
+    $b = 7;
+    $c = $a * $b;
+    return "Hasil Perkalian = " . (string)$c . "</h1>";
+}
+
+}
